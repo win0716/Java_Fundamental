85114c6c7f6aa49f1bea1f2b7fd6e5c2de879d00
win0716 <gg706gg@naver.com>
2019-07-18 17:59:14 +0900

    2019-07-18-v2

---
+++ src/java_20190718/CalenderDamo.java
@@ -0,0 +1,65 @@
+package java_20190718;
+
+public class CalenderDamo {
+	public static void main(String[] arge) {
+		/*
+		 * -달력 만들기 1. 1년 1월 1일 월요일 
+		 * 2. 1년 365일이고, 윤년일 때는 366일 - 2월 29일 
+		 * 3. 윤년은 4년마다 발생하고
+		 * 그중에서 100의 배수는 제외하고 400배수는 제외하지 않는다. 
+		 * 4. 2019년 12월 25일 무슨요일 일까요? 5. 2018년까지 총
+		 * 일수, 11월까지 총일수, 25일 더해서 7로 나눈 나머지가 1이면 월요일, 2이면 화요일....
+		 * 
+		 */
+
+		int year = 2020;
+		int month = 12;
+		int day = 25;
+		// preYear => 2018년도
+		int preYear = year - 1;
+		// preMonth => 2019년 11월
+		int preMonth = month - 1;
+		int totalCount = 0;
+
+		// 2018년 까지 총 일 수 구하기
+		totalCount = preYear * 365 + (preYear / 4 - preYear / 100 + preYear / 400);
+		
+		int[] monthArray= {31,28,31,30,31,30,31,31,30,31,30,31};
+		
+		//boolean isLeafYear = year%4==0 && (year%100 !=0 || year%400 == 0);
+		//if(isLeafyear) {
+		//    monthArray[1] = 29;
+		
+		if(year%4==0 && (year%100 !=0 || year%400 == 0)) {
+			monthArray[1] = 29;
+		}
+
+		// 2019년도 11월까지 합 구하기
+		for(int i=0; i<preMonth;i++) {
+			totalCount += monthArray[i];
+		}
+
+		totalCount += day;
+
+		int dayOfWeek = totalCount % 7;
+		String message = null;
+		if (dayOfWeek == 1) {
+			message = "월요일";
+		} else if (dayOfWeek == 2) {
+			message = "화요일";
+		} else if (dayOfWeek == 3) {
+			message = "수요일";
+		} else if (dayOfWeek == 4) {
+			message = "목요일";
+		} else if (dayOfWeek == 5) {
+			message = "금요일";
+		} else if (dayOfWeek == 6) {
+			message = "토요일";
+		} else if (dayOfWeek == 0) {
+			message = "일요일";
+		}
+		System.out.println(year + "년\t " + month + "월 " + day + "일 " + message);
+
+	}
+
+}
--- src/java_20190718/LotteryDemo2.java
+++ src/java_20190718/LotteryDemo2.java
@@ -2,20 +2,31 @@ package java_20190718;
 
 public class LotteryDemo2 {
 	public static void main(String[] agrs) {
-		//math.random() => 0 보다 크가나 같고 1보다 작은 양수를
-		//double 값으로 반환한다.
 		
+		for (int k = 0; k <5; k++) {
+			int[] lotto = new int[6];
+			
+			for(int i = 0 ; i<lotto.length;i++) {
+				double random = math.random();
+				in
+			}
+		}
 		
-	
+			
+			
+			
+			
 		int[] lotto = new int[6];
 		
-		outter : for(int i = 0 ; i<lotto.length;i++) {
+		for(int i = 0 ; i<lotto.length;i++) {
 			double random = Math.random();
-			int temp = (int)(random * 45) + 1;
+			int temp = (int)(random * 45) + 1;{
+			int lotto[i] != temp;
+			}
 			lotto[i] = temp;
-			if(lotto[i] != lotto[i]) break outter;   				
+             	
 						} 
-	
+	 
 			  		
 		
 		for(int i = 0; i<lotto.length; i++) {
