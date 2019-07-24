fd7e78430cb88e7bc50879198c9c45dd8c669fe0
win0716 <gg706gg@naver.com>
2019-07-24 18:04:38 +0900

    java-20190724_v2

--- src/java_20190722/Member.java
+++ src/java_20190722/Member.java
@@ -9,11 +9,12 @@ public class Member {
 	private String ssn2;
 	private int age;
 	private String regdate;
-	
-	//name => setter , getter 메서드
-	//name에 대한 getter 메서드 => 인스턴스 name 정보를 가져올수 있는 메서드
-	
-	//name 에 대한 setter 메서드 => 인스턴스 name 정보를 변경할 수 있는 메서드 
+
+	// name => setter , getter 메서드
+		// name에 대한 getter 메서드 => 인스턴스 name 정보를 가져올수 있는 메서드
+
+		// name 에 대한 setter 메서드 => 인스턴스 name 정보를 변경할 수 있는 메서드
+
 	
 	public String getName() {
 		return name;
@@ -21,49 +22,48 @@ public class Member {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public void setZipcode(String zipcode) {
-		this.zipcode = zipcode;
-	}
 	public String getZipcode() {
 		return zipcode;
 	}
-	public void setAddr(String Addr1) {
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getAddr1() {
+		return addr1;
+	}
+	public void setAddr1(String addr1) {
 		this.addr1 = addr1;
 	}
-	public String getAddr() {
-		return addr1;
+	public String getAddr2() {
+		return addr2;
 	}
 	public void setAddr2(String addr2) {
 		this.addr2 = addr2;
 	}
 	public String getSsn1() {
-		return ssn2;
+		return ssn1;
 	}
 	public void setSsn1(String ssn1) {
 		this.ssn1 = ssn1;
-		
-	}
-	public String getSsn1() {
-		return ssn1;
-	}
-	public void setSsn2(Sting Ssn2) {
-		this.ssn2 = ssn2;
 	}
 	public String getSsn2() {
-		return  ssn2;
+		return ssn2;
 	}
-	public void setAge(int age) {
-		this.age= age;
-	
+	public void setSsn2(String ssn2) {
+		this.ssn2 = ssn2;
 	}
-	publoc String getAge() {
+	public int getAge() {
 		return age;
 	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
 
-public String getRegdate() {
-	return regdate;
-}
-public void setRegdate(String regdate) {
-	this.regdate = regdate;
-}
+	
 }
--- src/java_20190722/MemberDemo.java
+++ src/java_20190722/MemberDemo.java
@@ -7,16 +7,18 @@ public class MemberDemo {
 		m.setName("성영한");
 		//Sysrem.out.println(m.name);
 		m.setZipcode("12345");
-		m.setAdd1("서울");
+		m.setAddr1("서울");
 		m.setAddr2("용산구");
 		m.setSsn1("111111");
-		m.sstSsn2("-1111118");
+		m.setSsn2("-1111118");
 		m.setAge(30);
 		m.setRegdate("2019-07-22");
 		System.out.println(m.getName());
+		System.out.println(m.getZipcode());
 		System.out.println(m.getAddr1());
 		System.out.println(m.getAddr2());
-		System.out.println(m.getSsn());
+		System.out.println(m.getSsn1());
+		System.out.println(m.getSsn2());
 		System.out.println(m.getAge());		
 		System.out.println(m.getRegdate());
 		
