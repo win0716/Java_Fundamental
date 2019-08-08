4337686034166b847ae695653e4155d774aa3a4d
winner <winner@192.168.219.103>
2019-08-09 00:48:16 +0900

    java_20190808_vHome

--- src/homework2/MemberFindDto.java
+++ src/homework2/MemberFindDto.java
@@ -9,10 +9,55 @@ public class MemberFindDto {
 	
 	
 	//생성자 만들기
-	public MemberFindDto(int seq//////)
-			super()
-			this.seq = seq;
-	//seq에 대하 setter 만들기
+	public MemberFindDto(int seq, String email, String code, String sdate, String edate) {
+		super();
+		this.seq = seq;
+		this.email= email;
+		this.code = code;
+		this.sdate = sdate;
+		this.edate = edate;
+		
+
+	}
+		//seq에 대하 setter 만들기
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public int getSeq() {
+		return seq;
+	}
+	
+	public void setEmail(String email) {
+		this.email =email;
+		
+	}
+	public String getEmail() {
+		return email;
+		
+	}
+	
+	public void setCode (String code) {
+		this.code = code;
+		
+	}
+	public String getCode() {
+		return code;
+		
+	}
+	public void setSdate(String sdate) {
+		this.sdate =sdate;
+		
+	}
+	public String getSdate() {
+		return sdate;
+	}
+	public void setEdate (String edate) {
+		this.edate = edate;
+		
+	}
+	public String getEdate() {
+		return edate;
+	}
 	
 	
 	//seq에 대한 getter 만들기
--- src/homework2/MemberFindDtoDemo.java
+++ src/homework2/MemberFindDtoDemo.java
@@ -10,21 +10,27 @@ public class MemberFindDtoDemo {
 		System.out.println(m1.getEmail());
 		System.out.println(m1.getCode());
 		System.out.println(m1.getSdate());
-		System.out.println(m1.getEdte());
+		System.out.println(m1.getEdate());
+		System.out.println();
 		//3. 객체의 내용을 수정해 본다.
 		m1.setSeq(1111);
 		m1.setEmail("syh1111@test.com");
-		m1.setcode("1231231");
+		m1.setCode("1231231");
 		m1.setSdate("2010901");
-		m1.sesEdate("201923132");
+		m1.setEdate("201923132");
 		//4. 객체의 내용을 출력해 본다.
 		System.out.println(m1.getSeq());
 		System.out.println(m1.getEmail());
 		System.out.println(m1.getCode());
 		System.out.println(m1.getSdate());
-		System.out.println(m1.getEdte());
+		System.out.println(m1.getEdate());
 		///1,2,3,4
-		MemberFindDto m2 = new MemberFindDto(2, "shy@teat.com", "111", "2019-08-01","2019-08-02");
+		MemberFindDto m2 = new MemberFindDto(2, "shy@teat.com", "222", "2019-08-02","2019-08-03");
+		System.out.println(m2.getSeq());
+		System.out.println(m2.getEmail());
+		System.out.println(m2.getCode());
+		System.out.println(m2.getSdate());
+		System.out.println(m2.getEdate());
 		
 		
 		
