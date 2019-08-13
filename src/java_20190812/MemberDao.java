561c7c1219a2a1160be2cf359f6a2a0c94e00c41
win0716 <gg706gg@naver.com>
2019-08-13 17:58:49 +0900

    java_20190813_v1

--- src/java_20190812/MemberDao.java
+++ src/java_20190812/MemberDao.java
@@ -4,7 +4,9 @@ package java_20190812;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class MemberDao {
 	
@@ -70,6 +72,116 @@ public class MemberDao {
 		return isSuccess;
 		
 	}
+	public boolean update(MemberDto m) {
+		Connection con =null;
+		PreparedStatement pstmt = null;
+		boolean isSuccess = false;
+		int index = 1;
+		
+		try {
+			con = DriverManager.getConnection(
+					"jdbc:mariadb://localhost/kic","kic12","kic12");
+			//바인딩 변수(?)는 반드시 컬럼 값에만 설정할 수 있다.
+			StringBuffer sql = new StringBuffer();
+			sql.append("UPDATE member ");
+			sql.append("SET NAME = ? , addr= ? ");
+			sql.append("WHERE num = ? ");
+			
+			pstmt = con.prepareStatement(sql.toString());
+			
+			pstmt.setString(index++,m.getName());
+			pstmt.setString(index++, m.getAddr());
+			pstmt.setInt(index, m.getNum());
+			
+			pstmt.executeUpdate();
+			isSuccess = true;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null) pstmt.close();
+				if(con != null) con.close();
+				
+
+			} catch (SQLException e2) {
+
+			}
+		}
+		return isSuccess;
+	}
+	public boolean delete(int num) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		boolean isSuccess = false;
+		int index =1;
+		try {
+			con = DriverManager.getConnection("jdbc:mariadb://localhost/kic", "kic12", "kic12");
+			
+			StringBuffer sql = new StringBuffer();
+			sql.append("DELETE FROM member " );
+			sql.append("WHERE num = ? ");
+			
+			pstmt = con.prepareStatement(sql.toString());
+			
+			pstmt.setInt(index, num);
+			
+			pstmt.executeUpdate();
+			isSuccess= true;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null) pstmt.close();
+				if(con != null) con.close();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return isSuccess;
+	}
+	public ArrayList<MemberDto> select(){
+		Connection con=null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<MemberDto> list = new ArrayList<MemberDto>();
+		int index=1;
+		try {
+			
+			con= DriverManager.getConnection(
+					"jdbc:mysql://localhost/kic?autoReconnect=true", "kic12", "kic12");
+			StringBuffer sql = new StringBuffer();
+			sql.append("SELECT num, NAME, addr ");
+			sql.append("FROM member");
+			
+			pstmt = con.prepareStatement(sql.toString());
+			
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				index =1;
+				int num = rs.getInt(index++);
+				String name = rs.getString(index++);
+				String addr = rs.getString(index);
+				list.add(new MemberDto(num, name, addr));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) rs.close();
+				if(con != null) con.close();
+				if(pstmt !=null) pstmt.close();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+	
 	
 
 
--- src/java_20190812/MemberDemo.java
+++ src/java_20190812/MemberDemo.java
@@ -1,17 +1,44 @@
 package java_20190812;
 
+import java.util.ArrayList;
+
 public class MemberDemo {
 	public static void main(String[] args) {
 		MemberDao mdao = MemberDao.getInstance();
-		MemberDto m1 = new MemberDto(20, "트럼프","미국");
-		boolean isSuccess = mdao.insert(m1);
-		if(isSuccess) {
-			System.out.println("성공");
-		}else {
-			System.out.println("실패");
-		}
+//		MemberDto m1 = new MemberDto(22, "트럼프1","미국1");
+//		boolean isSuccess = mdao.insert(m1);
+//		if(isSuccess) {
+//			System.out.println("넣기 성공");
+//		}else {
+//			System.out.println("넣기 실패");
+//		}
+//		
+//		m1= new MemberDto(22, "트럼프2", "미국2");
+//		isSuccess = mdao.update(m1);
+//		if(isSuccess) {
+//			System.out.println("수정 성공");
+//		}else {
+//			System.out.println("수정 실패");
+//		}
+//		
+//		
+//		isSuccess = mdao.delete(20);
+//		if(isSuccess) {
+//			System.out.println("삭제 성공");
+//		}else {
+//			System.out.println("삭제 실패");
+//		}
+			
+		ArrayList<MemberDto> list = mdao.select();
 		
-				
+		for(MemberDto m : list) {
+			int num = m.getNum();
+			String name = m.getName();
+			String addr = m.getAddr();
+					
+			System.out.printf("%d\t%s\t%s%n", num, name, addr);
+		}
 	}
+	
 
 }
